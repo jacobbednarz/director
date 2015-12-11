870d654230ea0076fb456e8a26c399407ad96a92
Jacob Bednarz <jacob.bednarz@gmail.com>
2015-12-11 17:47:18 +1100

    Add not_found and redirect overview

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 
 class Redirector < Sinatra::Base
+  not_found do
+    halt 404, 'Not found'
+  end
+
   get '/' do
     'homepage'
   end
 
   get '/:short_link' do
-    "i'm redirecting you to #{params[:short_link]}"
+    # Find shortlink in redis.
+    # Do `redirect "http://google.com"`
   end
 end
